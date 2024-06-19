e11a73f5903eeba7a1779a25ae8015f7665bf51d
Rishabh Gulecha <91297906+rishabh790453@users.noreply.github.com>
2024-06-19 05:00:40 -0500

    x
    x

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'File Picker and JSON Parsing Demo',
+      title: 'INSTAGRAM CHAT ANALYZER',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
