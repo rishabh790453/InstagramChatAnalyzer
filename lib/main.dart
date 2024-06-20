7dbbbc8ca07f657e651a9874b8c5b2fd6090972b
Rishabh Gulecha <91297906+rishabh790453@users.noreply.github.com>
2024-06-19 19:12:07 -0500

    l

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'INSTAGRAM CHAT ANALYZER',
+      title: 'Instagram Chat Analyzer',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
@@ -28,18 +28,37 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   List<dynamic> participants = [];
   List<dynamic> messages = [];
-  List<dynamic> user1mess = [];
-  List<dynamic> user2mess = [];
+  
   List<dynamic> participant1Messages = [];
   List<dynamic> participant2Messages = [];
 
-  String user1x = '';
-  String user2x = '';
-
+  List<int> totaltimesuser1 = [];
+  List<int> totaltimesuser2 = [];
+  double avgtimesuser1 = 0;
+  double avgtimesuser2 = 0;
+  int sumtimesuser1 = 0;
+  int sumtimesuser2 = 0;
+  
   Map<String, int> messageCount = {};
   Map<String, double> averageResponseTime = {};
 
   void _openFilePicker() async {
+    // Resetting state
+    setState(() {
+      participants = [];
+      messages = [];
+      participant1Messages = [];
+      participant2Messages = [];
+      totaltimesuser1 = [];
+      totaltimesuser2 = [];
+      avgtimesuser1 = 0;
+      avgtimesuser2 = 0;
+      sumtimesuser1 = 0;
+      sumtimesuser2 = 0;
+      messageCount = {};
+      averageResponseTime = {};
+    });
+
     FilePickerResult? result = await FilePicker.platform.pickFiles(
       type: FileType.custom,
       allowedExtensions: ['json'],
@@ -49,18 +68,13 @@ class _MyHomePageState extends State<MyHomePage> {
       String jsonContent = await File(result.files.single.path!).readAsString();
       Map<String, dynamic> jsonData = jsonDecode(jsonContent);
 
-      // Process messages and remove 'share' field
       List<dynamic> processedMessages = _processMessages(jsonData['messages']);
 
       setState(() {
         participants = jsonData['participants'];
         messages = processedMessages;
         _calculateMessageCounts();
-        user1x = participants.isNotEmpty ? participants[0]['name'] : '';
-        user2x = participants.length > 1 ? participants[1]['name'] : '';
       });
-    } else {
-      // User canceled the picker
     }
   }
 
@@ -80,7 +94,7 @@ class _MyHomePageState extends State<MyHomePage> {
     return processedMessages;
   }
 
- void _calculateMessageCounts() {
+  void _calculateMessageCounts() {
     messageCount.clear();
     averageResponseTime.clear();
 
@@ -90,7 +104,6 @@ class _MyHomePageState extends State<MyHomePage> {
     }
 
     for (var message in messages) {
-      print(message);
       String senderName = message['sender_name'];
 
       if (senderName == participants[0]['name']) {
@@ -103,32 +116,25 @@ class _MyHomePageState extends State<MyHomePage> {
     messageCount[participants[0]['name']] = participant1Messages.length;
     messageCount[participants[1]['name']] = participant2Messages.length;
 
-    for (var participant in participants) {
-      String participantName = participant['name'];
-      List<dynamic> participantMessages = participantName == participants[0]['name'] ? participant1Messages : participant2Messages;
-
-      if (participantMessages.isNotEmpty) {
-        double totalResponseTime = 0.0;
-        int responseCount = 0;
-        String lastSender = '';
-
-        for (int i = 1; i < participantMessages.length; i++) {
-          String senderName = participantMessages[i]['sender_name'];
-
-          if (senderName == participantName) {
-            int timeDifferenceMs = participantMessages[i]['timestamp_ms'] - participantMessages[i - 1]['timestamp_ms'];
-            double timeDifferenceMin = timeDifferenceMs / 60000.0;
-
-            totalResponseTime += timeDifferenceMin;
-            responseCount++;
-          }
-        }
-
-        if (responseCount > 0) {
-          averageResponseTime[participantName] = totalResponseTime / responseCount;
+    for (int i = messages.length - 1; i > 0; i--) {
+      if (messages[i]['sender_name'] != messages[i - 1]['sender_name']) {
+        if (messages[i - 1]['sender_name'] == participants[0]['name']) {
+          totaltimesuser1.add((messages[i - 1]['timestamp_ms'] - messages[i]['timestamp_ms']));
+        } else {
+          totaltimesuser2.add((messages[i - 1]['timestamp_ms'] - messages[i]['timestamp_ms']));
         }
       }
     }
+
+    for (int x in totaltimesuser1) {
+      sumtimesuser1 += x;
+    }
+    for (int x in totaltimesuser2) {
+      sumtimesuser2 += x;
+    }
+
+    avgtimesuser1 = (sumtimesuser1 / (totaltimesuser1.length * 60000));
+    avgtimesuser2 = (sumtimesuser2 / (totaltimesuser2.length * 60000));
   }
 
   @override
@@ -140,7 +146,7 @@ class _MyHomePageState extends State<MyHomePage> {
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: <Widget>[
             ElevatedButton(
               onPressed: _openFilePicker,
@@ -152,21 +158,15 @@ class _MyHomePageState extends State<MyHomePage> {
               style: TextStyle(fontWeight: FontWeight.bold),
             ),
             SizedBox(height: 10),
-            ListView.builder(
-              shrinkWrap: true,
-              itemCount: participants.length,
-              itemBuilder: (context, index) {
-                String participantName = participants[index]['name'];
-                int messageCount = index == 0 ? participant1Messages.length : participant2Messages.length;
-                
-                return Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Text('$participantName'),
-                    Text('$messageCount'),
-                  ],
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: participants.map((participant) {
+                int messageCount = participant['name'] == participants[0]['name'] ? participant1Messages.length : participant2Messages.length;
+                return Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 4.0),
+                  child: Text('${participant['name']}: $messageCount'),
                 );
-              },
+              }).toList(),
             ),
             SizedBox(height: 20),
             Text(
@@ -174,21 +174,16 @@ class _MyHomePageState extends State<MyHomePage> {
               style: TextStyle(fontWeight: FontWeight.bold),
             ),
             SizedBox(height: 10),
-            ListView.builder(
-              shrinkWrap: true,
-              itemCount: participants.length,
-              itemBuilder: (context, index) {
-                String participantName = participants[index]['name'];
-                double avgResponseTime = averageResponseTime[participantName] ?? 0.0;
-
-                return Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Text('$participantName'),
-                    Text(avgResponseTime.toStringAsFixed(2)),
-                  ],
-                );
-              },
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  '${participants.isNotEmpty ? participants[0]['name'] : 'User 1'}: ${participants.isNotEmpty ? avgtimesuser1.toStringAsFixed(2) : ''} minutes',
+                ),
+                Text(
+                  '${participants.isNotEmpty ? participants[1]['name'] : 'User 2'}: ${participants.isNotEmpty ? avgtimesuser2.toStringAsFixed(2) : ''} minutes',
+                ),
+              ],
             ),
           ],
         ),
